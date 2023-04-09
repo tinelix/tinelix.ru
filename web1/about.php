b8e12441cd4f644392714b7ba461b1eb90bee588
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-04-09 23:34:00 +0700

    Update about.php

--- web1/about.php
+++ web1/about.php
@@ -4,6 +4,6 @@ require dirname(__FILE__) . '/cms/pages.php';
 $db = new SQLite3(dirname(__FILE__) . '/cms/pub.db');
 genPageHeader($_GET['encoding']);
 genWebsiteMenu($_GET['encoding']);
-showAboutPage($db, $html_encoding);
+showAboutPage($db, $_GET['encoding']);
 closePage($_GET['encoding']);
 ?>
