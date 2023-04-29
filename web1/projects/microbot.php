07c5aab107e8fa97e6725f74ca81028643e1972a
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-04-29 22:42:52 +0700

    removed

--- web1/projects/microbot.php
+++
@@ -1,8 +0,0 @@
-<?php
-require dirname(__FILE__) . '/../cms/core.php';
-require dirname(__FILE__) . '/../cms/pages.php';
-genPageHeader($_GET['encoding']);
-genWebsiteMenu($_GET['encoding']);
-showProjectPage(1, $_GET['encoding']);
-closePage($_GET['encoding']);
-?>
