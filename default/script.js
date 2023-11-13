315fd91e027f1bb6f8180b5f2d9c9d018ad6b7a6
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-11-13 15:31:07 +0700

    Add footers

--- default/script.js
+++ default/script.js
@@ -24,3 +24,27 @@ var ee = "";
 document.onkeypress = function(event){
     ee = ee + event.key;
 }
+
+function newYearCountdownTimer() {
+    var today = new Date();
+    var newYear = new Date('1 January 2024');
+    var days = today - newYear;
+    var count_time = new Date(-days + (today.getTimezoneOffset() * 60 * 1000));
+    var countdown = Math.floor(-(days / 1000 / 60 / 60 / 24));
+    if(countdown < 0) {
+        document.getElementById('days_count').innerHTML = '0';
+        return;
+    }
+    var timezone = '';
+    if((today.getTimezoneOffset() / 60) < 0) {
+        timezone = '+' + (-today.getTimezoneOffset() / 60)
+    } else {
+        timezone = '-' + (today.getTimezoneOffset() / 60)
+    };
+    document.getElementById('days_count').innerHTML = countdown;
+}
+
+function homepage_onload() {
+    setInterval(newYearCountdownTimer, 100);
+}
+
