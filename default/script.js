f42dcd84a4935a3e1e6061d033e2ec51eeae8e8f
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-12-30 22:15:26 +0700

    add detalized ny timer

--- default/script.js
+++ default/script.js
@@ -30,21 +30,35 @@ function newYearCountdownTimer() {
     var newYear = new Date('1 January 2024');
     var days = today - newYear;
     var count_time = new Date(-days + (today.getTimezoneOffset() * 60 * 1000));
-    var countdown = Math.floor(-(days / 1000 / 60 / 60 / 24));
-    if(countdown < 0) {
-        document.getElementById('days_count').innerHTML = '0';
-        return;
-    }
-    var timezone = '';
-    if((today.getTimezoneOffset() / 60) < 0) {
-        timezone = '+' + (-today.getTimezoneOffset() / 60)
-    } else {
-        timezone = '-' + (today.getTimezoneOffset() / 60)
-    };
-    document.getElementById('days_count').innerHTML = countdown;
+    var countdown = Math.floor(-(days / 1000 / 60 / 60 / 60)) + 1;
+    if(countdown >= 0) {
+    	var countdown_h = addIntZeros(Math.floor(-(days / 1000 / 60 / 60)) % 24, 2);
+    	var countdown_min = addIntZeros(Math.floor(-(days / 1000 / 60)) % 60, 2);
+    	var countdown_sec = addIntZeros(Math.floor(-(days / 1000)) % 60, 2);
+    	var timezone = '';
+    	if((today.getTimezoneOffset() / 60) < 0) {
+        	timezone = '+' + (-today.getTimezoneOffset() / 60)
+   	} else {
+        	timezone = '-' + (today.getTimezoneOffset() / 60)
+    	};
+	document.getElementById('days_count').innerHTML = countdown;
+    	document.getElementById('hours_count').innerHTML = countdown_h;
+    	document.getElementById('minutes_count').innerHTML = countdown_min;
+    	document.getElementById('seconds_count').innerHTML = countdown_sec;
+   } else {
+	document.getElementById('days_count').innerHTML = "0";
+  	document.getElementById('hours_count').innerHTML = "00";
+    	document.getElementById('minutes_count').innerHTML = "00";
+    	document.getElementById('seconds_count').innerHTML = "00";
+   }
 }
 
 function homepage_onload() {
     setInterval(newYearCountdownTimer, 100);
 }
 
+function addIntZeros(num, size) {
+    num = num.toString();
+    while (num.length < size) num = "0" + num;
+    return num;
+}
