4cfae11351be35a25d4fc1c20373389f7e3bc22c
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-12-31 18:31:46 +0700

    Update core.php

--- web1/cms/core.php
+++ web1/cms/core.php
@@ -78,11 +78,11 @@
         $tz = "Europe/Moscow";
         $dt = new DateTime("now", new DateTimeZone($tz)); //first argument "must" be a string
         $dt->setTimestamp(time());
-        $new_year_countdown = -round(($dt->getTimestamp() - strtotime("2024-01-01")) / (60 * 60 * 24));
+        $new_year_countdown = -round(($dt->getTimestamp() - strtotime("2024-01-01 00:00:00")) / (60 * 60 * 24));
         if($new_year_countdown >= 0) {
-            $new_year_countdown_h = -(round(($dt->getTimestamp() - strtotime("2024-01-01")) / (60 * 60)) % 24);
-            $new_year_countdown_min = -(round(($dt->getTimestamp() - strtotime("2024-01-01")) / 60) % 60);
-            $new_year_countdown_sec = -(round($dt->getTimestamp() - strtotime("2024-01-01")) % 60);
+            $new_year_countdown_h = -(round(($dt->getTimestamp() - strtotime("2024-01-01 00:00:00")) / (60 * 60)) % 24);
+            $new_year_countdown_min = -(round(($dt->getTimestamp() - strtotime("2024-01-01 00:00:00")) / 60) % 60);
+            $new_year_countdown_sec = -(round($dt->getTimestamp() - strtotime("2024-01-01 00:00:00")) % 60);
         } else {
             $new_year_countdown = 0;
             $new_year_countdown_h = 0;
