bf5f3e8215c824ca3112067696e006bc905660c4
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-01-02 21:20:29 +0700

    Update core.php

--- web1/cms/core.php
+++ web1/cms/core.php
@@ -207,7 +207,7 @@
         \r\n                            <a href=\"https://narodweb.ru\">
         \r\n                                <img style=\"border:0;width:88px;height:31px\"
         \r\n                                     src=\"http://".$web1_subdomain."/banners/ndr.gif\"
-        \r\n                                     alt=\"Сайт проекта &qt;Народное достояние Рунета&lt;\" />
+        \r\n                                     alt=\"Сайт проекта &lt;Народное достояние Рунета&gt;\" />
         \r\n                            </a>
         \r\n                        </td>
         \r\n                    </tr>
