579b0411d49c254d6276daeecdb71f6237e82883
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-01-25 12:27:27 +0700

    Update pages.php

--- web1/cms/pages.php
+++ web1/cms/pages.php
@@ -293,7 +293,8 @@
             }
             $html = $html."</div>
                 \r\n                    </td>
-                \r\n                </tr>";
+                \r\n                </tr>
+                \r\n            </table>";
         if(!$html_encoding || $html_encoding != "utf-8") {
             echo mb_convert_encoding($html, "windows-1251", "utf-8");
         } else {
