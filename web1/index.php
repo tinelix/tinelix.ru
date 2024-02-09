5c94778e48caf8a00379d48bd8a711268edab74d
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-02-09 15:12:55 +0700

    Update index.php

--- web1/index.php
+++ web1/index.php
@@ -5,10 +5,10 @@
     $db = new SQLite3(dirname(__FILE__) . '/cms/pub.db');
 
     $encoding = "";
-    if($_GET['encoding'] != null) {
-        $encoding = $_GET['encoding'];
-    } else {
+    if($_GET['encoding'] ?? null) {
         $encoding = "utf-8";
+    } else {
+        $encoding = $_GET['encoding'];
     }
 
     loadTemplate($db, $encoding, showStartPage($db, $_GET['encoding']));
