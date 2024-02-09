faadcf44f0a0f44e295ee3112f702833ab882357
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-02-09 15:56:33 +0700

    Update index.php

--- web1/index.php
+++ web1/index.php
@@ -2,7 +2,7 @@
     require dirname(__FILE__) . '/cms/src/core.php';
     require dirname(__FILE__) . '/cms/src/pages.php';
 
-    $db = new SQLite3(dirname(__FILE__) . '/cms/pub.db');
+    $db = new SQLite3(dirname(__FILE__) . '/cms/src/pub.db');
 
     $encoding = "utf-8";
 
