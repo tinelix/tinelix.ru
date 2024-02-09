7aaeb16e7adf60659d21073b17d83802cdaccbfb
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-02-09 17:03:26 +0700

    Remove Start and About page to pages.php

--- web1/cms/pages.php
+++ web1/cms/pages.php
@@ -1,4 +1,65 @@
 <?php
+
+    function showStartPage($db, $html_encoding) {
+        $web1_subdomain = "web1.tinelix.ru";
+        $irc_subdomain = "irc.tinelix.ru";
+        $query = "SELECT id, title, body FROM articles WHERE on_start_page = 1 LIMIT 1;";
+        $result = $db->query($query);
+        $article = $result->fetchArray();
+        $article_title = mb_strtoupper($article[1]);
+        $html =  "\r\n                    <td bgcolor=\"#151515\" valign=\"top\">
+            \r\n                        <div class=\"title-text\">".$article_title."</div>
+            \r\n                        <hr class=\"accent-color\" size=\"1\">
+            \r\n                        <div class=\"text\">".$article[2]."</div>
+            \r\n                    </td>
+            \r\n                </tr>
+            \r\n            </tbody>
+            \r\n        </table>";
+        if(!$html_encoding || $html_encoding != "utf-8") {
+            echo mb_convert_encoding($html, "windows-1251", "utf-8");
+        } else {
+            echo $html;
+        }
+    }
+
+    function showAboutPage($db, $html_encoding) {
+        $web1_subdomain = "web1.tinelix.ru";
+        $irc_subdomain = "irc.tinelix.ru";
+        $query = "SELECT id, title, body FROM about LIMIT 1;";
+        $result = $db->query($query);
+        $about_page = $result->fetchArray();
+        $about_page_title = mb_strtoupper($about_page[1]);
+        $html = "
+            \r\n                    <td bgcolor=\"#151515\" valign=\"top\">
+            \r\n                        <div class=\"title-text\">".$about_page_title."</div>
+            \r\n                        <hr class=\"accent-color\" size=\"1\"/>
+            \r\n                        <div class=\"text\">
+            \r\n                            ".$about_page[2];
+        $html = $html."\r\n               <h3>Контакты</h3>";
+        $query = "SELECT id, name, value FROM contacts;";
+        $result = $db->query($query) or die("Last error: {$db->lastErrorMsg()}\n");
+        $contacts = array();
+        while($contact = $result->fetchArray()) {
+            array_push($contacts, $contact);
+        }
+        for($i = 0; $i < count($contacts); ++$i) {
+            $html = $html."
+            \r\n                            <b>".$contacts[$i][1].":</b> ".$contacts[$i][2]."<br>";
+        }
+        $html = $html."
+            \r\n
+            \r\n                        </div>
+            \r\n                    </td>
+            \r\n                </tr>
+            \r\n            </tbody>
+            \r\n        </table>";
+        if(!$html_encoding || $html_encoding != "utf-8") {
+            echo mb_convert_encoding($html, "windows-1251", "utf-8");
+        } else {
+            echo $html;
+        }
+    }
+
     function showProjectsPage($db, $html_encoding) {
         $query = "SELECT id, name, description, link FROM projects;";
         $result = $db->query($query) or die("Last error: {$db->lastErrorMsg()}\n");
