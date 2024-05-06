9052c8305edbe4b6f00226d8706c2e982da8e395
Dmitry Tretyakov <tretdm@inbox.ru>
2024-05-06 17:36:53 +0700

    Refactor project pages

--- web1/projects.php
+++ web1/projects.php
@@ -5,9 +5,11 @@ $db = new SQLite3(dirname(__FILE__) . '/cms/pub.db');
 genPageHeader($_GET['encoding']);
 genWebsiteMenu($db, $_GET['encoding']);
 if($_GET['page']) {
-    showProjectPage($_GET['page'], $_GET['encoding']);
+    showProjectPage($db, $_GET['page'], $_GET['encoding']);
 } else {
     showProjectsPage($db, $_GET['encoding']);
 }
 closePage($_GET['encoding']);
+$db->close();
+unset($db);
 ?>
